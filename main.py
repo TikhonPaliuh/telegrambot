08559c8080fe5edffee8852e64ce260980eb21e2
TikhonPaliuh <t1paluh@gmail.com>
2026-02-15 20:34:58 +0100

    Version 0.0.1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello world")
+import os 
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+	await update.message.reply_text(f"Hello {update.effective_user.first_name}")
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
